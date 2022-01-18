b73e9065555866590b7c3ce15191b8714f7378f2
DzmitrySha <ravenbydev@gmail.com>
2022-01-18 22:17:55 +0300

    added cli.py, modified brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+from brain_games.cli import welcome_user
 
 def greet(who):
     print(f'Welcome to the {who}!')
 
 def main():
     greet('Brain games')
+    welcome_user()
 
 
 if __name__== '__main__':
