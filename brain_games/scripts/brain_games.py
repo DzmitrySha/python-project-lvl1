d9d0868b5d251febad3ff39f6ef993e38ccf55b0
DzmitrySha <ravenbydev@gmail.com>
2022-01-22 20:57:03 +0300

    mod brain_even brain_games, del pycache

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -13,7 +13,6 @@ def welcome_user():
 
     """
     print('Welcome to the Brain games!')
-    global name
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!'.format(name))
     return name
