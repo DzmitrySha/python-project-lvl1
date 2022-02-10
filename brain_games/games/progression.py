a2f12c9fa6c3fc7bcf885a3feb53d90a851a97fc
DzmitrySha <ravenbydev@gmail.com>
2022-02-10 19:48:03 +0300

    made the make_progression function clean

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,11 +5,10 @@ from random import randint
 DESCRIPTION = 'What number is missing in the progression?'
 
 
-def make_progression():
+def make_progression(progression_len,
+                     min_step, max_step,
+                     min_first_num, max_first_num):
     """Generate arithmetic progression."""
-    progression_len = 10
-    min_step, max_step = 1, 10
-    min_first_num, max_first_num = 0, 20
     first_num = randint(min_first_num, max_first_num)
     progression_step = randint(min_step, max_step)
     progression = [first_num, ]
@@ -22,7 +21,13 @@ def make_progression():
 
 def make_question_and_correct_answer():
     """ Make game question and answer."""
-    progression = make_progression()
+    progression = make_progression(
+        progression_len=10,
+        min_step=1,
+        max_step=10,
+        min_first_num=0,
+        max_first_num=20
+    )
     random_index = randint(0, len(progression) - 1)
     correct_answer = progression[random_index]
     progression[random_index] = '..'
