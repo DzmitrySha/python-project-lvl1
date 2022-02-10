b46da6b3d8f7cdd18327fb50c5a943216b0e77a4
DzmitrySha <ravenbydev@gmail.com>
2022-02-10 19:38:10 +0300

    allocate is_prime function

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,15 +6,19 @@ DESCRIPTION = 'Answer "yes" if given number is prime. ' \
               'Otherwise answer "no".'
 
 
+def is_prime(number):
+    if number == 1:
+        return "no"
+    for i in range(2, (number // 2 + 1)):
+        if number % i == 0:
+            return "no"
+    return "yes"
+
+
 def make_question_and_correct_answer():
     """Make game question and answer."""
     min_number = 1
     max_number = 21
     number = randint(min_number, max_number)
     question = str(number)
-    if number == 1:
-        return question, "no"
-    for i in range(2, (number // 2 + 1)):
-        if number % i == 0:
-            return question, "no"
-    return question, "yes"
+    return question, is_prime(number)
